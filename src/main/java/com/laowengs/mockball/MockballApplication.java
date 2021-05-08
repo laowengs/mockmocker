612c9057d1bbde5e9f270f1554fbe97ed0327bc7
laowengs <gh13003898942>
2021-05-08 16:10:02 +0800

    init

---
+++ src/main/java/com/laowengs/mockball/MockballApplication.java
@@ -0,0 +1,13 @@
+package com.laowengs.mockball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockballApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockballApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/laowengs/mockball/MockballApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laowengs.mockball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockballApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

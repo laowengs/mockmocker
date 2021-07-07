e275ef0b19d7d43a782a319072a7ebc9aeb6f057
laowengs <369196439@qq.com>
2021-07-07 22:27:30 +0800

    ssl

--- src/test/java/com/laowengs/mocker/MockerApplicationTests.java
+++ src/test/java/com/laowengs/mocker/MockerApplicationTests.java
@@ -1,13 +1,155 @@
 package com.laowengs.mocker;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
+import org.apache.http.ParseException;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.LayeredConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.AllowAllHostnameVerifier;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.http.ssl.SSLContexts;
+import org.apache.http.ssl.TrustStrategy;
+import org.apache.http.util.EntityUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.client.support.BasicAuthenticationInterceptor;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootTest
+import javax.net.ssl.SSLContext;
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
 class MockerApplicationTests {
 
     @Test
-    void contextLoads() {
+    void getExecute() {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        try {
+            // 创建httpget.
+            HttpGet httpget = new HttpGet("https://127.0.0.1:31443/");
+            System.out.println("executing request " + httpget.getURI());
+            // 执行get请求.
+            CloseableHttpResponse response = httpclient.execute(httpget);
+            try {
+                // 获取响应实体
+                HttpEntity entity = response.getEntity();
+                System.out.println("--------------------------------------");
+                // 打印响应状态
+                System.out.println(response.getStatusLine());
+                if (entity != null) {
+                    // 打印响应内容长度
+                    System.out.println("Response content length: " + entity.getContentLength());
+                    // 打印响应内容
+                    System.out.println("Response content: " + EntityUtils.toString(entity));
+                }
+                System.out.println("------------------------------------");
+            } finally {
+                response.close();
+            }
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            // 关闭连接,释放资源
+            try {
+                httpclient.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
+    public static CloseableHttpClient createIgnoreSSLHttpClient() {
+        try {
+            SSLContext sslContext = new SSLContextBuilder().loadTrustMaterial(null, new TrustStrategy() {
+                public boolean isTrusted(X509Certificate[] chain,
+                                         String authType) throws CertificateException {
+                    return true;
+                }
+            }).build();
+            SSLConnectionSocketFactory sslConnectionSocketFactory = new SSLConnectionSocketFactory(sslContext, NoopHostnameVerifier.INSTANCE);
+            return HttpClients.custom().setSSLSocketFactory(sslConnectionSocketFactory).build();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    @Test
+    void getExecuteIgnoreSSl() throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException, IOException {
+
+        CloseableHttpClient client = createIgnoreSSLHttpClient();
+        HttpGet httpget = new HttpGet("https://127.0.0.1:31443/");
+        System.out.println("executing request " + httpget.getURI());
+        CloseableHttpResponse response = client.execute(httpget);
+        try {
+            // 获取响应实体
+            HttpEntity entity = response.getEntity();
+            System.out.println("--------------------------------------");
+            // 打印响应状态
+            System.out.println(response.getStatusLine());
+            if (entity != null) {
+                // 打印响应内容长度
+                System.out.println("Response content length: " + entity.getContentLength());
+                // 打印响应内容
+                System.out.println("Response content: " + EntityUtils.toString(entity));
+            }
+            System.out.println("------------------------------------");
+        } finally {
+            response.close();
+        }
+
+    }
+
+
+    @Test
+    void restTemplateIgnoreSSl() throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
+        RestTemplate restTemplate = new RestTemplate(httpComponentsClientHttpRequestFactory());
+
+        ResponseEntity<String> forEntity = restTemplate.getForEntity("https://127.0.0.1:31443/", String.class);
+        System.out.println(forEntity.getBody());
+    }
+
+
+
+    public HttpComponentsClientHttpRequestFactory httpComponentsClientHttpRequestFactory() throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
+        TrustStrategy acceptingTrustStrategy = (x509Certificates, authType) -> true;
+        SSLContext sslContext = SSLContexts.custom().loadTrustMaterial(null, acceptingTrustStrategy).build();
+        LayeredConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext, new NoopHostnameVerifier());
+
+        Registry<ConnectionSocketFactory> sfr = RegistryBuilder.<ConnectionSocketFactory>create().register("http", PlainConnectionSocketFactory.getSocketFactory()).register("https", csf != null ? csf : SSLConnectionSocketFactory.getSocketFactory()).build();
+        PoolingHttpClientConnectionManager pollingConnectionManager = new PoolingHttpClientConnectionManager(sfr);
+        pollingConnectionManager.setMaxTotal(100);
+        pollingConnectionManager.setDefaultMaxPerRoute(100);
+
+        CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(csf).setConnectionManager(pollingConnectionManager).build();
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        factory.setHttpClient(httpClient);
+        factory.setConnectTimeout(100);
+        factory.setReadTimeout(100);
+        factory.setConnectionRequestTimeout(100);
+
+        return factory;
+    }
 }

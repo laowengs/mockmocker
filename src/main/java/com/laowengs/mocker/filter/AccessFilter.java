bc935d8d4853b068d40560ff7462d2e9b22cc82e
laowengs <Iweng.me2295>
2021-07-22 09:04:30 +0800

    增加过滤器，打印访问日志

---
+++ src/main/java/com/laowengs/mocker/filter/AccessFilter.java
@@ -0,0 +1,42 @@
+package com.laowengs.mocker.filter;
+
+import com.laowengs.mocker.servlet.MockServlet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@WebFilter(filterName = "AccessFilter")
+public class AccessFilter implements Filter {
+    private static final Logger logger = LoggerFactory.getLogger(MockServlet.class);
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        /*初始化方法  接收一个FilterConfig类型的参数 该参数是对Filter的一些配置*/
+        logger.debug("AccessFilter init");
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        /*过滤方法 主要是对request和response进行一些处理，然后交给下一个过滤器或Servlet处理*/
+        //交给下一个过滤器或servlet处理
+        HttpServletRequest req = (HttpServletRequest)request;
+        String remoteAddr = request.getRemoteAddr();
+        logger.info(" ip {} call uri {}",remoteAddr,req.getRequestURI());
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+        /*销毁时调用*/
+        logger.debug("AccessFilter destroy");
+    }
+}
